ed0a4e5f3c07d9243cae6c2d996752f1a395c0e8
Ubuntu <ubuntu@ip-172-31-43-85.us-west-2.compute.internal>
2021-06-19 18:24:46 +0000

    fix setup

--- transformers/examples/seq2seq/generate_xsum_files.py
+++ transformers/examples/seq2seq/generate_xsum_files.py
@@ -17,5 +17,5 @@ def write_file(dataset, output_dir, file_name):
 
 if __name__ == "__main__":
     write_file(dataset['test'], output_dir, 'test')
-    write_file(dataset['validation'], output_dir, 'validation')
+    write_file(dataset['validation'], output_dir, 'val')
     write_file(dataset['train'], output_dir, 'train')
