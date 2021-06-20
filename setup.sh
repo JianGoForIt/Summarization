1eb131fcbf7931e964403dee7247427cb7bb7a46
Ubuntu <ubuntu@ip-172-31-43-85.us-west-2.compute.internal>
2021-06-20 21:29:00 +0000

    ready for prefix run

---
+++ setup.sh
@@ -0,0 +1,24 @@
+conda env create -f environment.yml
+conda activate prefix-tuning
+
+pushd transformer
+pip install -e .
+popd
+
+pip install datasets
+pip install pytorch-lightning==0.8.5
+pip install gitpython
+pip install rouge-score==0.0.4
+pip install sacrebleu==1.4.14
+
+pushd ../
+git clone https://github.com/NVIDIA/apex
+pushd apex
+pip install -v --disable-pip-version-check --no-cache-dir --global-option="--cpp_ext" --global-option="--cuda_ext" ./
+popd
+popd
+
+pushd transformer/examples/seq2seq
+mkdir ./xsum/raw_text
+python generate_xsum_files.py
+popd
